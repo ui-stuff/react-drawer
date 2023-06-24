10f0324ae15a21a62f4db8cefbf0b2e2657bdb5d
trykie <131900979+trykie@users.noreply.github.com>
2023-06-23 20:02:22 -0600

    Add ESLint-Prettier

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { DrawerProps } from './Drawer.types'
 
 const Drawer = ({ children }: DrawerProps) => {
-    return (
-        <div>{children}</div>
-    )
+    return <div>{children}</div>
 }
 
 export default Drawer
